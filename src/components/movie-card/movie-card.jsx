3b1e43c7212580bc3320f6e3a402ef050850b1da
zoe-rowe8 <zoemarierowe@hotmail.co.uk>
2023-03-03 14:16:50 +0000

    Styling with React Bootstrap

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,14 +1,20 @@
 import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
+
+import "./movie-card.scss";
 
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
-    <div
-      onClick={() => {
-        onMovieClick(movie);
-      }}
-    >
-      {movie.Title}
-    </div>
+    <Card className="h-100">
+      <Card.Img variant="top" src={movie.ImagePath} />
+      <Card.Body>
+        <Card.Title>{movie.Title}</Card.Title>
+        <Card.Text>{movie.Description}</Card.Text>
+        <Button onClick={() => onMovieClick(movie)} variant="link">
+          Open
+        </Button>
+      </Card.Body>
+    </Card>
   );
 };
 
